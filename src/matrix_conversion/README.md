2b4df0afe91758236794860c558372a9b821f4d5
tnie <tnie@wustl.edu>
2024-06-25 22:18:00 -0500

    matrix conversion

--- src/matrix_conversion/README.md
+++ src/matrix_conversion/README.md
@@ -3,7 +3,7 @@
 Intel i9-13900K, Windows 11
 
 ```
-2024-06-24T15:44:12-05:00
+2024-06-25T22:16:29-05:00
 Running C:\Users\tnie\code\microbenches\build\win64\src\matrix_conversion\Release\matrix_conversion_benchmark.exe
 Run on (32 X 2995 MHz CPU s)
 CPU Caches:
@@ -11,19 +11,28 @@ CPU Caches:
   L1 Instruction 32 KiB (x16)
   L2 Unified 2048 KiB (x16)
   L3 Unified 36864 KiB (x1)
----------------------------------------------------------------------------
-Benchmark                                 Time             CPU   Iterations
----------------------------------------------------------------------------
-BM_ArmadilloConversion/256            13993 ns         6094 ns       100000
-BM_ArmadilloConversion/512           335702 ns       212054 ns         5600
-BM_ArmadilloConversion/4096        21692453 ns     17463235 ns           34
-BM_HandRolledConversion/256           18689 ns        10463 ns        89600
-BM_HandRolledConversion/512           77106 ns        49734 ns        14452
-BM_HandRolledConversion/4096        9601958 ns      4799107 ns          140
-BM_OpenMPConversion/256                3682 ns         3617 ns       263529
-BM_OpenMPConversion/512               10798 ns         9068 ns        89600
-BM_OpenMPConversion/4096            4219969 ns      3676471 ns          187
-BM_OpenCVParallelConversion/256       21258 ns        18589 ns        34462
-BM_OpenCVParallelConversion/512       30113 ns        27274 ns        26353
-BM_OpenCVParallelConversion/4096    3538342 ns      3138951 ns          224
+------------------------------------------------------------------------------
+Benchmark                                    Time             CPU   Iterations
+------------------------------------------------------------------------------
+BM_ArmadilloConversion/256               13242 ns         9267 ns        80929
+BM_ArmadilloConversion/512              365968 ns       258092 ns         2240
+BM_ArmadilloConversion/4096           24568550 ns     17708333 ns           30
+BM_HandRolledConversion/256              20174 ns        12556 ns        56000
+BM_HandRolledConversion/512              80300 ns        50000 ns        10000
+BM_HandRolledConversion/4096           9759993 ns      6944444 ns           90
+BM_OpenMPConversion/256                   5543 ns         5625 ns       100000
+BM_OpenMPConversion/512                  13345 ns        12870 ns        49778
+BM_OpenMPConversion/4096               4303319 ns      5000000 ns          100
+BM_OpenCVParallelConversion1/256         20355 ns        19496 ns        34462
+BM_OpenCVParallelConversion1/512         29814 ns        30134 ns        28000
+BM_OpenCVParallelConversion1/4096      3527950 ns      3244174 ns          236
+BM_OpenCVParallelConversion2/256         10447 ns        11475 ns        64000
+BM_OpenCVParallelConversion2/512         18806 ns        18032 ns        40727
+BM_OpenCVParallelConversion2/4096      3501157 ns      3216912 ns          204
+BM_OpenCVMatConversion/256                6702 ns         4653 ns       154483
+BM_OpenCVMatConversion/512               39179 ns        27344 ns        28000
+BM_OpenCVMatConversion/4096            8680204 ns      4823826 ns          149
+BM_OpenCVMatParallelConversion/256       10115 ns        10045 ns        74667
+BM_OpenCVMatParallelConversion/512       18062 ns        18032 ns        40727
+BM_OpenCVMatParallelConversion/4096    3422001 ns      3125000 ns          280
 ```
--- src/matrix_conversion/matrix_conversion_benchmark.cpp
+++ src/matrix_conversion/matrix_conversion_benchmark.cpp
@@ -29,8 +29,8 @@ void OpenMPConversion(const arma::Mat<uint16_t> &input,
 }
 
 // Function to convert matrix using OpenCV's cv::parallel_for_
-void OpenCVParallelConversion(const arma::Mat<uint16_t> &input,
-                              arma::Mat<double> &output) {
+void OpenCVParallelConversion1(const arma::Mat<uint16_t> &input,
+                               arma::Mat<double> &output) {
   output.set_size(input.n_rows, input.n_cols);
   cv::parallel_for_(cv::Range(0, input.n_elem), [&](const cv::Range &range) {
     for (int i = range.start; i < range.end; ++i) {
@@ -39,55 +39,102 @@ void OpenCVParallelConversion(const arma::Mat<uint16_t> &input,
   });
 }
 
-const auto conversion_bench = [](const auto &callable) {
-  return [&](benchmark::State &state) {
-    arma::Mat<uint16_t> input(state.range(0), state.range(0),
-                              arma::fill::randu);
-    arma::Mat<double> output;
-    for (auto _ : state) {
-      callable(input, output);
+// Function to convert matrix using OpenCV's cv::parallel_for_
+void OpenCVParallelConversion2(const arma::Mat<uint16_t> &input,
+                               arma::Mat<double> &output) {
+  output.set_size(input.n_rows, input.n_cols);
+  cv::parallel_for_(cv::Range(0, input.n_cols), [&](const cv::Range &range) {
+    for (int col = range.start; col < range.end; ++col) {
+      const auto *inptr = input.colptr(col);
+      auto *outptr = output.colptr(col);
+      for (int i = 0; i < input.n_rows; ++i) {
+        outptr[i] = static_cast<double>(inptr[i]);
+      }
     }
-  };
-};
+  });
+}
 
-// Benchmark for Armadillo conversion
-static void BM_ArmadilloConversion(benchmark::State &state) {
+void OpenCVMatConversion(const cv::Mat &input, cv::Mat &output) {
+  input.convertTo(output, CV_64F);
+}
+
+void OpenCVMatParallelConversion(const cv::Mat &input, cv::Mat &output) {
+  if (output.rows != input.rows || output.cols != input.cols ||
+      output.type() != CV_64F) {
+    input.convertTo(output, CV_64F);
+    return;
+  }
+
+  // Use OpenCV's parallel_for_ to parallelize the conversion
+  cv::parallel_for_(cv::Range(0, input.rows), [&](const cv::Range &range) {
+    for (int i = range.start; i < range.end; ++i) {
+      const uint16_t *inputRowPtr = input.ptr<uint16_t>(i);
+      double *outputRowPtr = output.ptr<double>(i);
+      for (int j = 0; j < input.cols; ++j) {
+        outputRowPtr[j] = static_cast<double>(inputRowPtr[j]);
+      }
+    }
+  });
+}
+
+template <typename Func>
+void BenchmarkArmaFunc(benchmark::State &state, Func func) {
   arma::Mat<uint16_t> input(state.range(0), state.range(0), arma::fill::randu);
   arma::Mat<double> output;
   for (auto _ : state) {
-    ArmadilloConversion(input, output);
+    func(input, output);
   }
+}
+
+// Benchmark for Armadillo conversion
+static void BM_ArmadilloConversion(benchmark::State &state) {
+  BenchmarkArmaFunc(state, ArmadilloConversion);
 }
 BENCHMARK(BM_ArmadilloConversion)->Range(256, 4096);
 
 // Benchmark for hand-rolled conversion
 static void BM_HandRolledConversion(benchmark::State &state) {
-  arma::Mat<uint16_t> input(state.range(0), state.range(0), arma::fill::randu);
-  arma::Mat<double> output;
-  for (auto _ : state) {
-    HandRolledConversion(input, output);
-  }
+  BenchmarkArmaFunc(state, HandRolledConversion);
 }
 BENCHMARK(BM_HandRolledConversion)->Range(256, 4096);
 
 // Benchmark for OpenMP conversion
 static void BM_OpenMPConversion(benchmark::State &state) {
-  arma::Mat<uint16_t> input(state.range(0), state.range(0), arma::fill::randu);
-  arma::Mat<double> output;
-  for (auto _ : state) {
-    OpenMPConversion(input, output);
-  }
+  BenchmarkArmaFunc(state, OpenMPConversion);
 }
 BENCHMARK(BM_OpenMPConversion)->Range(256, 4096);
 
 // Benchmark for OpenCV parallel_for_ conversion
-static void BM_OpenCVParallelConversion(benchmark::State &state) {
-  arma::Mat<uint16_t> input(state.range(0), state.range(0), arma::fill::randu);
-  arma::Mat<double> output;
+static void BM_OpenCVParallelConversion1(benchmark::State &state) {
+  BenchmarkArmaFunc(state, OpenCVParallelConversion1);
+}
+BENCHMARK(BM_OpenCVParallelConversion1)->Range(256, 4096);
+
+static void BM_OpenCVParallelConversion2(benchmark::State &state) {
+  BenchmarkArmaFunc(state, OpenCVParallelConversion2);
+}
+BENCHMARK(BM_OpenCVParallelConversion2)->Range(256, 4096);
+
+template <typename Func>
+void BenchmarkCvFunc(benchmark::State &state, Func func) {
+  cv::Mat input(state.range(0), state.range(0), CV_16UC1);
+  cv::randu(input, 0, 1);
+  cv::Mat output(state.range(0), state.range(0), CV_64F);
   for (auto _ : state) {
-    OpenCVParallelConversion(input, output);
+    func(input, output);
   }
 }
-BENCHMARK(BM_OpenCVParallelConversion)->Range(256, 4096);
+
+// Benchmark for OpenCV parallel_for_ conversion
+static void BM_OpenCVMatConversion(benchmark::State &state) {
+  BenchmarkCvFunc(state, OpenCVMatConversion);
+}
+BENCHMARK(BM_OpenCVMatConversion)->Range(256, 4096);
+
+// Benchmark for OpenCV parallel_for_ conversion
+static void BM_OpenCVMatParallelConversion(benchmark::State &state) {
+  BenchmarkCvFunc(state, OpenCVMatParallelConversion);
+}
+BENCHMARK(BM_OpenCVMatParallelConversion)->Range(256, 4096);
 
 BENCHMARK_MAIN();
